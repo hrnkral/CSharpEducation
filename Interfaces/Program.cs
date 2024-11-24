fd88ad84605e4594cba2357ad1df1442e7394ff5
Harun Karal <hrnkral@gmail.com>
2024-11-24 18:21:28 +0300

    Interface and Interface demo

---
+++ Interfaces/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Customer customer = new Customer { Address = "Ankara", FirstName = "Engin", LastName = "Demiroğ", Id = 1 };
+            //PersonManager personManager = new PersonManager();
+            //Student student = new Student { Departmant = "Computer Science", FirstName = "Derin", LastName = "Demiroğ", Id = 2 };
+            //Student student2 = new Student { Departmant = "Bilgisayar Mühendisliği", FirstName = "Harun", LastName = "Karal", Id = 3 };
+
+            //personManager.Add(customer);
+            //personManager.Add(student);
+            //personManager.Add(student2);
+
+            //CustomerManager customerManager = new CustomerManager();
+            //customerManager.Add(new OracleCustomerDal());
+            //customerManager.Add(new SqlServerCustomerDal());
+
+            ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySqlCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+               
+            }
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+
+    interface IPerson
+    {
+         int Id { get; set; }
+         string FirstName { get; set; }
+         string LastName { get; set; }
+         string Departmant { get; set; }
+
+
+
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+
+        public string Address { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine("Kişinin Adı:{0}\nKişinin Soyadı:{1}\nKişinin id:{2}\nKişinin Departmanı:{3}",person.FirstName,person.LastName,person.Id,person.Departmant);
+         
+        }
+    }
+}
---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] worker = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach (var item in worker)
+            {
+                item.Work();
+            }
+
+            IEat[] eat = new IEat[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+
+            foreach (var item in eat)
+            {
+                item.Eat();
+            }
+
+            ISalary[] salary = new ISalary[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+
+            foreach (var item in salary)
+            {
+                item.GetSalary();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    interface IWorker 
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    interface IRobot 
+    {
+        void Work();
+    }
+
+    class Worker : IWorker, IEat, ISalary 
+    {
+        public void Work()
+        {
+            Console.WriteLine("Work");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("Eat");
+        }
+        public void GetSalary()
+        {
+            Console.WriteLine("Get Salary");
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Work");
+        }
+    }
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Work()
+        {
+            Console.WriteLine("Work");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("Eat");
+        }
+        public void GetSalary()
+        {
+            Console.WriteLine("Get Salary");
+        }
+    }
+}
