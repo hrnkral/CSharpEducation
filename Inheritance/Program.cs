33511ace3d54a0d8e57c39e7a1defedc0a024753
Harun Karal <hrnkral@gmail.com>
2024-11-26 17:50:19 +0300

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person[] persons = new Person[3];
+            {
+                new Customer { Name = "Harun" };
+                new Person { Name = "Said" };
+                new Student { Name = "Asya" };
+            }
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+
+
+
+
+
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Departmant { get; set; }
+    }
+
+
+}
---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Save();
+            sqlServer.Update();
+            MySql mySql = new MySql();
+            mySql.Save();
+            mySql.Update();
+            Console.ReadLine();
+
+        }
+    }
+
+    class Database
+    {
+        public virtual void Save()
+        {
+            Console.WriteLine("Database saved");
+        }
+
+        public virtual void Update()
+        {
+            Console.WriteLine("Database updated");
+        }
+    }
+
+    class SqlServer : Database
+    {
+        override public void Save()
+        {
+            //base.Save();
+            Console.WriteLine("SqlServer saved");
+        }
+
+    }
+
+    class MySql : Database
+    {
+        
+
+    }
+
+
+}
