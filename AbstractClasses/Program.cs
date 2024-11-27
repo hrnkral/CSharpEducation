f5629a3fed40757b9aa08f0acb400fac7de0fbc2
Harun Karal <hrnkral@gmail.com>
2024-11-27 20:23:44 +0300

    abstract and recapdemo2

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            sqlServer.Delete();
+            MySql mySql = new MySql();
+            mySql.Add();
+            mySql.Delete();
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Database
+    {
+        public  void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+
+        public abstract void Delete();
+    }
+
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Sql");
+        }
+    }
+
+    class MySql : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by MySql");
+        }
+    }
+
+
+}
