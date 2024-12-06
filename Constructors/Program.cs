cf74a53c33afd06878bc0b8cc0f8087553a23ea9
Harun Karal <hrnkral@gmail.com>
2024-12-06 20:53:28 +0300

    ctor and referance and value type

---
+++ Constructors/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(10);
+            //customerManager.Add();
+            customerManager.List();
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class CustomerManager 
+    {
+        private int _count;
+        public CustomerManager(int count)
+        {
+            _count = count;
+
+        }
+        public void List() 
+        {
+            Console.WriteLine("Listed {0} items", _count);
+        }
+        public void Add() 
+        {
+            Console.WriteLine("Added!");
+        }
+    }
+}
---
+++ ReferanceAndValueTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferanceAndValueTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+
+            sayi1 = sayi2;
+
+            sayi2 = 65;
+
+            Console.WriteLine("1.Sayı:{0}",sayi1);
+
+            string[] cities1 = { "Ankara", "Adana", "Afyon" };
+            string[] cities2 = { "Bursa", "Bolu", "Balıkesir" };
+
+            cities1 = cities2;
+
+            cities1[0] = "İstanbul";
+
+            Console.WriteLine(cities1[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
